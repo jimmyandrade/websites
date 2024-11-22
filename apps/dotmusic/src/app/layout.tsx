59e372ab06a9b7f8ddabf0b9af7b9799bdea9422
Jimmy Andrade <contato@jimmyandrade.com>
2024-11-22 13:20:07 -0300

    feat: update RootLayout with enhanced metadata, navigation links, and accessibility features

--- apps/dotmusic/src/app/layout.tsx
+++ apps/dotmusic/src/app/layout.tsx
@@ -1,11 +1,13 @@
-import { Heading, Theme } from '@radix-ui/themes';
+import { Container, Heading, Theme, VisuallyHidden } from '@radix-ui/themes';
+import Link from 'next/link';
+import '@radix-ui/themes/styles.css';
 import './global.css';
 
-const title = 'Jimmy Andrade';
-
 export const metadata = {
-  title,
-  description: 'Website oficial de Jimmy Andrade',
+  title:
+    'Jimmy Andrade - Website oficial: Música, Agenda de Shows, Vídeos, Notícias e Biografia',
+  description:
+    'Website oficial do cantor, compositor e produtor musical Jimmy Andrade. Artista de indie, pop e rock brasileiro. Escute suas músicas, consulte sua agenda e obtenha outras informações sobre a sua música.',
 };
 
 export default function RootLayout({
@@ -14,11 +16,39 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <Theme asChild>
+    <Theme asChild hasBackground>
       <html lang="pt-br">
-        <body>
+        <body className="bg-black text-white">
           <header>
-            <Heading as="h1">{title}</Heading>
+            <Container>
+              <Heading as="h1">
+                <Link href="/">Jimmy Andrade</Link>
+              </Heading>
+              <VisuallyHidden asChild>
+                <a href="#content">Pular para o conteúdo principal</a>
+              </VisuallyHidden>
+              <nav aria-label="Navegação">
+                <ul>
+                  <li>
+                    <Link href="/">Início</Link>
+                  </li>
+                  <li>
+                    <Link href="/musica">Música</Link>
+                  </li>
+                  <li>
+                    <Link href="/bio">
+                      Bio&nbsp;e&nbsp;
+                      <em className="not-italic" lang="en">
+                        Releases
+                      </em>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="/midia">Mídia</Link>
+                  </li>
+                </ul>
+              </nav>
+            </Container>
           </header>
           <main id="content" role="main" tabIndex={-1}>
             {children}
