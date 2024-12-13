85006feaf4f574335125db9b62bf4ef459ec40fe
Jimmy Andrade <contato@jimmyandrade.com>
2024-12-13 14:20:51 -0300

    feat: add SocialMediaLinks component with links to various platforms

---
+++ apps/dotmusic/src/features/social-media-links/SocialMediaLinks.tsx
@@ -0,0 +1,52 @@
+import { Flex } from '@radix-ui/themes';
+
+export const SocialMediaLinks = () => (
+  <Flex asChild>
+    <ul>
+      <li>
+        <a
+          href="https://instagram.com/jimmyandrade"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Instagram
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://youtube.com/jimmyandrade"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          YouTube
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://tiktok.com/@jimmyandrademusic"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          TikTok
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://open.spotify.com/intl-pt/artist/5cXCuau8jCVmsRoFYj5oGB"
+          rel="noopener noreferrer"
+        >
+          Spotify
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://music.apple.com/br/artist/jimmy-andrade/678083982"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Apple Music
+        </a>
+      </li>
+    </ul>
+  </Flex>
+);
